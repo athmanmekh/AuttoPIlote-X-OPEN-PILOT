6a37d28a4bbf77f4473006800133ea8cc0d48da3
drNymbus <xxvmelanconxx@gmail.com>
2020-02-17 14:17:26 +0100

    syntax errors fixed

--- unite-centrale/UC.java
+++ unite-centrale/UC.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 // import java.io.DataInputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.StringReader;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,14 +17,14 @@ import javax.json.JsonObject;
 
 public class UC {
     // peut etre mettre la commande en attribut en cas d'echec d'envoi vers l'autopilote
-    private ArrayList<JsonObject> commands = new ArrayList<JSONObject>();
-    private String status = "send";
+    private static ArrayList<JsonObject> commands = new ArrayList<JsonObject>();
+    private static String status = "send";
 
     // {"id" : int,"command" : string,"metadata" : {"x" : float/double,"y" : float/double,"z" : float/double }}
     public static JsonObject reformat(JsonObject obj) {
         String s_command = "";
-        JsonObject meta;
-        if (gcs.getBoolean("buttonLeftPressed")) {
+        JsonObject meta = null;
+        if (obj.getBoolean("buttonLeftPressed")) {
             s_command = "LEFT";
         } else if (obj.getBoolean("buttonRightPressed")) {
             s_command = "RIGHT";
@@ -33,14 +35,14 @@ public class UC {
         } else if (obj.getBoolean("isNewPosition")) {
             s_command = "GOTO";
             meta = Json.createObjectBuilder()
-                .add("x", obj.getDouble("x"))
-                .add("y", obj.getDouble("y"))
-                .add("z", obj.getDouble("z"))
+                .add("x", obj.getJsonNumber("x").doubleValue())
+                .add("y", obj.getJsonNumber("y").doubleValue())
+                .add("z", obj.getJsonNumber("z").doubleValue())
                 .build();
         }
 
         JsonObject res = Json.createObjectBuilder()
-            .add("id", cmd.getInt("id"))
+            .add("id", obj.getInt("id"))
             .add("command", s_command)
             .add("metadata", meta)
             .build();
@@ -56,27 +58,27 @@ public class UC {
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 
-            if (who == null) String who = in.readLine();
+            if (who == null) who = in.readLine();
 
             if (who == "GCS") {
-                JsonReader jsonReader = Json.createReader(in.readObject());
+                JsonReader jsonReader = Json.createReader(new StringReader(in.readLine()));
                 JsonObject gcs = jsonReader.readObject();
                 jsonReader.close();
 
-                this.commands.add(gcs);
+                commands.add(gcs);
             }
 
             if (who == "AP") {
                 if (status == "send") {
-                    JsonObject cmd = this.commands.get(0);
-                    JsonObject cmd = this.reformat(cmd);
+                    JsonObject cmd = commands.get(0);
+                    cmd = reformat(cmd);
 
                     out.writeChars(cmd.toString());
                     status = "check";
 
                 } else if (status == "check") {
                     String s = in.readLine();
-                    if (s == "ok") this.commands.remove(0);
+                    if (s == "ok") commands.remove(0);
 
                     status = "send";
 
