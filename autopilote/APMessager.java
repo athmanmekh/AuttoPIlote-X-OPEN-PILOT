8d093fbeb3072e9a96ae76c71de83ff20e39fd0a
drNymbus <xxvmelanconxx@gmail.com>
2020-03-25 17:41:11 +0100

    soon la premiere demo, ça a l'air de fonctionner mais j'ai pas pu faire de test poussé

--- autopilote/APMessager.java
+++ autopilote/APMessager.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
+// import java.io.Reader;
+// import java.io.StringReader;
 import java.io.DataOutputStream;
 import java.io.DataInputStream;
 // import java.io.ObjectInputStream;
@@ -13,124 +13,109 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import javax.json.Json;
-import javax.json.JsonReader;
-import javax.json.JsonReaderFactory;
-import javax.json.JsonObject;
-import javax.json.JsonArray;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 import capteurs.Capteur;
 
-public class APMessager {
+public class APMessager extends APUtils {
 	private static Socket bus;
 	private static DataOutputStream bus_out;
 	private static DataInputStream bus_in;
 	private static AP ap;
 
-	// private static Capteur capteurs1;
-	// private static JsonReaderFactory readFactory = Json.createReaderFactory(Map<String,?>);
-
-	// [{
-	// "id" : int,
-	// "command" : string,
-	// "metadata" : {
-	// "x" : float/double,
-	// "y" : float/double,
-	// "z" : float/double
-	// }
-	// }, ... ]
-	private static ArrayList<JsonObject> commands = new ArrayList<JsonObject>();
-
-	// {
-	// "position" : {"x" : float, "y" : float, "z" : float},
-	// "contact" : {"f" : float, "b" : float, "l" : float, "r" : float}
-	// }
-	private static JsonObject capteurs = null;
-	
-	private static boolean sendBusMSG( JsonObject instruction, DataOutputStream bus_out ) throws IOException {
-
-	private static void sendBusMSG(JsonObject obj) throws IOException {
+	private static void sendBusMSG(JSONObject obj) throws IOException {
 		if (bus.isConnected()) {
 			try {
+				System.out.println("SENDING : " + obj.toString());
 				String cmd = "SEND " + obj.toString();
-				bus_out.writeBytes(cmd);
+				bus_out.writeBytes(cmd + '\n');
 			} catch (IOException e) {
-				System.out.println("Error sending : " + obj.toString());
+				System.out.print("Error sending " + obj.toString() + ": ");
 				System.out.println(e.getMessage());
 			}
 		}
 	}
 
 	@SuppressWarnings("deprecation")
-	private static JsonArray getBusMSG(String id) throws IOException {
+	private static JSONArray getBusMSG(String id) throws IOException {
 		if (bus.isConnected()) {
 			try {
+				System.out.println("GET FROM : " + id);
 				String cmd = "GET " + id;
-				bus_out.writeBytes(cmd);
+				bus_out.writeBytes(cmd + '\n');
 
 				String resp = bus_in.readLine();
-				JsonReader jsonReader = Json.createReader(new StringReader(resp));
-				JsonArray arr = jsonReader.readArray();
-				jsonReader.close();
+				JSONArray arr = new JSONArray(resp);
 
+				System.out.println("RESPONSE : " + arr.toString());
 				return arr;
 			} catch (IOException e) {
-				System.out.println("Error getting from : " + id);
+				System.out.print("Error getting from " + id + ": ");
 				System.out.println(e.getMessage());
 			}
 		}
 		return null;
 	}
 
-	// met a jour la liste de commande actuelle avec celle recu en parametre
-	private static void fillCommands(JsonArray cmds) {}
-
-	// reformatte les donnees des capteurs recu, stockant le resultat dans l'attribut capteurs
-	private static void fillCapteurs(JsonArray sensors) {}
-
 	public static void main(String[] args) throws IOException, UnknownHostException {
+		ap = new AP();
 		try {
 			// open connection with BUS
-			bus = new Socket("TBD", 7777);
+			bus = new Socket("localhost", 7777);
 			bus_out = new DataOutputStream(bus.getOutputStream());
 			bus_in = new DataInputStream(bus.getInputStream());
+
 		} catch (UnknownHostException e) {
 			System.out.println("Unknown Host: " + e.getMessage());
 		}
 
 		try {
+			JSONObject id = new JSONObject();
+			id.put("Type", "autopilote");
+			id.put("TTL", "0");
+			System.out.println("IDENTIFICATION: " + id.toString());
+			bus_out.writeBytes(id.toString() + '\n');
+
 			while (bus.isConnected()) {
-				if (ap.getCommand() == Command.NONE) {
+				System.out.println("\nBUS IS CONNECTED");
+				if (ap.getCommand() == Command.WAIT) {
 
 					// * On récupère toutes les commandes sur le bus
-					JsonArray uc_commands = getBusMSG("UC");
+					JSONArray uc_commands = getBusMSG("UC");
 					// * On met a jour notre liste de commande, si l'id est déjà présent dans notre liste on ignore
 					// sinon on insere (insert sort) la/les commande(s) dans la liste.
 					fillCommands(uc_commands);
 
 					// * On recupere les donnees des capteurs (depuis le bus)
-					JsonArray new_capteurs = getBusMSG("CAPTEURS");
+					JSONObject new_capteurs = getBusMSG("CAPTEURS").getJSONObject(0); //parce qu'on recoit un JSONArray
 					fillCapteurs(new_capteurs);
 
+					// * On recupere la premiere commande de la liste
+					JSONObject cmd_obj = getNextCommand();
+					Command e_cmd = str2cmd(cmd_obj.getString("command"));
+					JSONObject meta = cmd_obj.getJSONObject("metadata");
+
 					// * On initialise l'AP
-					ap.init(Command.NONE, null, null);
+					ap.init(Command.WAIT, getCapteurs(), null);
 
 				} else {
 					// * On recupere les donnees des capteurs (depuis le bus)
-					JsonArray new_capteurs = getBusMSG("CAPTEURS");
+					JSONObject new_capteurs = getBusMSG("CAPTEURS").getJSONObject(0); //parce qu'on recoit un JSONArray
 					fillCapteurs(new_capteurs);
 
 					// * On update l'AP
-					ap.update(null);
+					ap.update(getCapteurs());
 				}
 
-				ap.compute()
+				ap.compute();
 				// * Ensuite on cree l'instruction grâce a l'AP
-				JsonObject instruction = ap.createInstruction();
+				JSONObject instruction = ap.createInstruction();
 				// * On pose l'instruction sur le bus
 				sendBusMSG(instruction);
 			}
 		} catch (IOException e) {
-			System.out.println("Unknown Host: " + e.getMessage());
+			System.out.println("IOException: " + e.getMessage());
 		}
-
+	}
+}
--- autopilote/capteurs/Position.java
+++ autopilote/capteurs/Position.java
@@ -5,44 +5,24 @@ public class Position extends Capteur {
         super(3);
     }
 
-    public Position(float x, float y, float z, float target_x, float target_y, float target_z) {
-        super(3);
+    public void setX(float x) { this.setData(0, x); }
+    public void setY(float y) { this.setData(1, y); }
+    public void setZ(float z) { this.setData(2, z); }
 
-        this.data[0] = x;
-        this.data[1] = y;
-        this.data[2] = z;
+    public float getX() { return this.getData(0); }
+    public float getY() { return this.getData(1); }
+    public float getZ() { return this.getData(2); }
 
-        this.target[0] = target_x;
-        this.target[1] = target_y;
-        this.target[2] = target_z;
-    }
+    public void setTargetX(float x) { this.setTarget(0, x); }
+    public void setTargetY(float y) { this.setTarget(1, y); }
+    public void setTargetZ(float z) { this.setTarget(2, z); }
 
-    public Position(float x, float y, float z) {
-        super(3);
+    public float getTargetX() { return this.getTarget(0); }
+    public float getTargetY() { return this.getTarget(1); }
+    public float getTargetZ() { return this.getTarget(2); }
 
-        this.data[0] = x;
-        this.data[1] = y;
-        this.data[2] = z;
-    }
-
-    public void setX(float x) { this.data[0] = x; }
-    public void setY(float y) { this.data[1] = y; }
-    public void setZ(float z) { this.data[2] = z; }
-
-    public float getX() { return this.data[0]; }
-    public float getY() { return this.data[1]; }
-    public float getZ() { return this.data[2]; }
-
-    public void setTargetX(float x) { this.target[0] = x; }
-    public void setTargetY(float y) { this.target[1] = y; }
-    public void setTargetZ(float z) { this.target[2] = z; }
-
-    public float getTargetX() { return this.target[0]; }
-    public float getTargetY() { return this.target[1]; }
-    public float getTargetZ() { return this.target[2]; }
-
-    public float getDiffX() { return this.diff[0]; }
-    public float getDiffY() { return this.diff[1]; }
-    public float getDiffZ() { return this.diff[2]; }
+    public float getDiffX() { return this.getDiff(0); }
+    public float getDiffY() { return this.getDiff(1); }
+    public float getDiffZ() { return this.getDiff(2); }
 
 }
